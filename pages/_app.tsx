cdb5e2b1e0094e8b648f6fd5e92019fd7fb1c7d8
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2021-11-30 21:53:58 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,27 @@
-import '../styles/globals.css'
-import '../styles/main.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Router from 'next/router'
-import NProgress from 'nprogress'
+import "../styles/globals.css";
+import "../styles/main.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
 // import NProgress from 'nprogress' //nprogress module
-import 'nprogress/nprogress.css' //styles of nprogress
+import "nprogress/nprogress.css"; //styles of nprogress
 //Binding events.
-NProgress.configure({ showSpinner: false })
-Router.events.on('routeChangeStart', () => NProgress.start())
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    < div >
+    <div>
       <Head>
         <title>Groovinx</title>
         <meta name="theme-color" content="#EC6448" />
         <link rel="icon" href="/logo.png" />
-        <meta
-          name="description"
-          content="Groovinx App"
-        />
+        <meta name="description" content="Groovinx App" />
       </Head>
       <Component {...pageProps} />
-    </div >
-  )
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
